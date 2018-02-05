b1c90135fe4c5f9a85269d968385e53182e017dd
Benjie <benjiecodes@gmail.com>
2018-02-05 16:16:17 -0500

    imported and exported created modules

--- module-examples/app.js
+++ module-examples/app.js
@@ -2,6 +2,15 @@ import {
     uniq
 } from 'lodash';
 import $ from 'jquery';
+import defaultApiKey from './src/config';
+import {
+    url,
+    sayHi
+} from './src/config';
+console.log(defaultApiKey, +' : ' + url);
+
+sayHi('Benjie')
+
 
 const ages = [1, 41, 42, 42, 5];
 
--- module-examples/src/config.js
+++ module-examples/src/config.js
@@ -1,1 +1,10 @@
-const apiKey = 'abc'
+const apiKey = 'abc123';
+
+export default apiKey; // because you exported it as default, you can name apiKey import as whatever you want
+
+export const url = 'https://github.com/benjiecodes' // because it's not default you must name it as url when imported in a {} bracket
+
+export function sayHi(name) {
+    console.log(`Hello there ${name}`);
+
+}
