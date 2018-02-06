55c3cae18e65d207bbc2f327f2d72fb8ea54699d
Benjie <benjiecodes@gmail.com>
2018-02-06 15:24:53 -0500

    added a quick test environment where server is client-side

--- toolfree-package-imports-using-systemjs/main.js
+++ toolfree-package-imports-using-systemjs/main.js
@@ -2,4 +2,9 @@ import {
     kebabCase
 } from 'npm:lodash';
 
+import {
+    addTax
+} from './checkout';
+
 console.log(kebabCase('benjie is using kebab case on lodash without npm installing anything'));
+console.log(addTax(100, 0.20));
